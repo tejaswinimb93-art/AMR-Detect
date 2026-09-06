3abc98d9dafea56af33b3ac7ed51f363614ba43f
tejaswinimb93-art <tejaswinimb93@gmail.com>
2026-09-06 22:15:15 +0530

    Add FASTA upload to AMR-Detect

--- app/app.py
+++ app/app.py
@@ -1,20 +1,6 @@
 import os
 import gradio as gr
 
-def analyze_sample(organism, sample_id):
-    if not organism:
-        return "Please select an organism."
-    
-    if not sample_id:
-        sample_id = "Not provided"
-    
-    return f"""Sample received successfully.
-
-Sample ID: {sample_id}
-Organism: {organism}
-
-AMR analysis will be performed here.
-"""
 
 organisms = [
     "Escherichia coli",
@@ -27,6 +13,29 @@ organisms = [
     "Other / Not listed"
 ]
 
+
+def analyze_sample(organism, sample_id, fasta_file):
+    if not organism:
+        return "Please select an organism."
+
+    if not fasta_file:
+        return "Please upload a FASTA file."
+
+    if not sample_id:
+        sample_id = "Not provided"
+
+    filename = os.path.basename(fasta_file)
+
+    return f"""Sample received successfully.
+
+Sample ID: {sample_id}
+Organism: {organism}
+FASTA file: {filename}
+
+AMR analysis engine will be connected in the next step.
+"""
+
+
 demo = gr.Interface(
     fn=analyze_sample,
     inputs=[
@@ -38,6 +47,11 @@ demo = gr.Interface(
         gr.Textbox(
             label="Sample ID",
             placeholder="Example: AMR001"
+        ),
+        gr.File(
+            label="Upload FASTA sequence",
+            file_types=[".fa", ".fasta", ".fna"],
+            type="filepath"
         )
     ],
     outputs=gr.Textbox(
@@ -47,6 +61,7 @@ demo = gr.Interface(
     description="Antimicrobial Resistance Detection and Analysis",
     flagging_mode="never"
 )
+
 
 port = int(os.environ.get("PORT", "10000"))
 
